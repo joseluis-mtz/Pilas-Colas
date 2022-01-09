64fad6269cd332aa356f1503332a2725d0d828d7
Jose Luis <joseluismartinez1024@gmail.com>
2022-01-08 20:21:38 -0600

    Cambiar form de inicio.

--- Pilas_Colas/Program.cs
+++ Pilas_Colas/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Pilas_Colas.Usuario;
 
 namespace Pilas_Colas
 {
@@ -16,7 +17,7 @@ namespace Pilas_Colas
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmInterfaz());
         }
     }
 }
